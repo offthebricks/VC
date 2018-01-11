0a33d86e2dec30118987143d23a027e58e8ee3b7
OffTheBricks <mircerlancerous@users.noreply.github.com>
2018-01-11 15:27:37 -0800

    Update VC.js

--- VC.js
+++ VC.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 OffTheBricks - https://github.com/mircerlancerous/jsLight
+Copyright 2018 OffTheBricks - https://github.com/mircerlancerous/jsLight
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
